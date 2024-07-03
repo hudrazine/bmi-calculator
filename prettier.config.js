14f6de58d4bc0d6967e04d0669956cfa6f437b56
hudrazine <39614590+hudrazine@users.noreply.github.com>
2024-07-03 05:08:28 +0000

    prettier設定ファイルの変更

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+/** @type {import('prettier').Options} */
+const config = {
+	useTabs: true,
+	printWidth: 100,
+	semi: false,
+	trailingComma: 'all',
+	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
+	plugins: ['prettier-plugin-svelte', '@trivago/prettier-plugin-sort-imports'],
+	importOrder: [
+		'^(svelte/(.*)$)|^(svelte$)',
+		'^(@sveltejs/(.*)$)|^(@sveltejs/kit$)',
+		'<THIRD_PARTY_MODULES>',
+		'^$lib/(.*)$',
+		'^[./]'
+	],
+	importOrderSeparation: true
+};
+
+export default config;
