9e626654d52ba9f155252695f0bb3ba82b50599f
hudrazine <39614590+hudrazine@users.noreply.github.com>
2024-07-03 08:38:13 +0000

    BMI計算コンポーネントの雛形を作成

---
+++ src/lib/BmiCalculator.svelte
@@ -0,0 +1,8 @@
+<svelte:options customElement="bmi-calculator" />
+
+<script lang="ts">
+	import { writable } from "svelte/store"
+
+	const height = writable(160)
+	const weight = writable(60)
+</script>
