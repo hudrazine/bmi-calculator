686839bf7916bbf9e5a7f7fa6fbddb7069ed2616
hudrazine <39614590+hudrazine@users.noreply.github.com>
2024-07-20 07:37:48 +0000

    ビルド設定の変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+// import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [svelte()],
+	build: {
+		lib: {
+			entry: 'src/lib/index.ts',
+			name: 'Shikumito/BmiCalculator',
+			fileName: (format) => `bmi-calculator.${format}.js`,
+			formats: ['es', 'umd']
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
