f5b4d2364ce4efe897927794d615cb00a322a54f
hudrazine <39614590+hudrazine@users.noreply.github.com>
2024-07-23 07:14:00 +0000

    ビルド生成ファイル名を`bmi-calculator`に変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { svelte } from "@sveltejs/vite-plugin-svelte"
+import { defineConfig } from "vite"
 
 export default defineConfig({
 	plugins: [svelte()],
 	build: {
 		rollupOptions: {
 			output: {
-				entryFileNames: 'assets/[name].js',
-				chunkFileNames: 'assets/[name].js',
-				assetFileNames: 'assets/[name].[ext]'
-			}
-		}
+				entryFileNames: "assets/bmi-calculator.js",
+				// chunkFileNames: 'assets/[name].js',
+				assetFileNames: "assets/bmi-calculator.[ext]",
+			},
+		},
 	},
 	server: {
 		watch: {
 			usePolling: true,
 			interval: 1000,
 		},
-		host: '0.0.0.0',
-		port: 5173
+		host: "0.0.0.0",
+		port: 5173,
 	},
-});
+})
