13078a60206758fa84c3cfa72496a428adc982b4
hudrazine <39614590+hudrazine@users.noreply.github.com>
2024-07-25 07:05:49 +0000

    lint & format

--- prettier.config.js
+++ prettier.config.js
@@ -3,17 +3,17 @@ const config = {
 	useTabs: true,
 	printWidth: 100,
 	semi: false,
-	trailingComma: 'all',
-	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
-	plugins: ['prettier-plugin-svelte', '@ianvs/prettier-plugin-sort-imports'],
+	trailingComma: "all",
+	overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
+	plugins: ["prettier-plugin-svelte", "@ianvs/prettier-plugin-sort-imports"],
 	importOrder: [
-		'^(svelte/(.*)$)|^(svelte$)',
-		'^(@sveltejs/(.*)$)|^(@sveltejs/kit$)',
-		'<THIRD_PARTY_MODULES>',
-		'^$lib/(.*)$',
-		'^[./]'
+		"^(svelte/(.*)$)|^(svelte$)",
+		"^(@sveltejs/(.*)$)|^(@sveltejs/kit$)",
+		"<THIRD_PARTY_MODULES>",
+		"^$lib/(.*)$",
+		"^[./]",
 	],
-	importOrderSeparation: true
-};
+	importOrderSeparation: true,
+}
 
-export default config;
+export default config
